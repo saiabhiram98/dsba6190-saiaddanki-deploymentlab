1ab2ae0e69e9de8e6f1b78df5fcc4f22cb48cbc0
Sai Abhiram Addanki <asaiabhiram@gmail.com>
2024-11-13 19:56:49 -0500

    main update

--- main.tf
+++ main.tf
@@ -42,3 +42,50 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
+// Virutal Network
+
+resource "azurerm_virtual_network" "example" {
+  name                = "vnet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "subnet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}""
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
+
+// SQL database and server
+
+resource "azurerm_mssql_server" "example" {
+  name                         = "sqlsvr-${var.class_name}-${var.student_name}-${var.environment}"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "example" {
+  name         = "sqldb-${var.class_name}-${var.student_name}-${var.environment}"
+  server_id    = azurerm_mssql_server.example.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
