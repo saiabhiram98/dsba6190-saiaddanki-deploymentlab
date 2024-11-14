897d2c880d0b60994f9af70aa861a2ebb376f0e6
Sai Abhiram Addanki <asaiabhiram@gmail.com>
2024-11-14 00:41:24 -0500

    update main

--- main.tf
+++ main.tf
@@ -40,6 +40,11 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
+  }
   tags = local.tags
 }
 
@@ -59,11 +64,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.2.0/24"]
   service_endpoints    = ["Microsoft.SQL", "Microsoft.Storage"]
 }
-network_rules {
-    default_action             = "Deny"
-    ip_rules                   = ["100.0.0.1"]
-    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
-}
+
 // SQL database and server
 
 resource "azurerm_mssql_server" "server" {
