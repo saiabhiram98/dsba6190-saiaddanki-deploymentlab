d17595452722d0524d7abeea13d77f3609f40980
Sai Abhiram Addanki <asaiabhiram@gmail.com>
2024-11-13 20:13:42 -0500

    update main

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vn.name
   address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints    = ["Microsoft.SQL","Microsoft.Storage"]
+  service_endpoints    = ["Microsoft.SQL", "Microsoft.Storage"]
 }
 
 // SQL database and server
