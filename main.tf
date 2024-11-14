22d92c0b552f1ce0bd398eaf81bf8622d83086f8
Sai Abhiram Addanki <asaiabhiram@gmail.com>
2024-11-13 20:11:05 -0500

    update main

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "azurerm_virtual_network" "vn" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}""
+  name                 = "subnet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vn.name
   address_prefixes     = ["10.0.2.0/24"]
