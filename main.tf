3502aa24e47032a8dc0b9f9e4ef0c6aa51cc8e94
Sai Abhiram Addanki <asaiabhiram@gmail.com>
2024-11-13 20:04:00 -0500

    update main

--- main.tf
+++ main.tf
@@ -45,35 +45,35 @@ resource "azurerm_storage_account" "storage" {
 
 // Virutal Network
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "vn" {
   name                = "vnet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "subnet" {
   name                 = "subnet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}""
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
   address_prefixes     = ["10.0.2.0/24"]
   service_endpoints    = ["Microsoft.Storage"]
 }
 
 // SQL database and server
 
-resource "azurerm_mssql_server" "example" {
+resource "azurerm_mssql_server" "server" {
   name                         = "sqlsvr-${var.class_name}-${var.student_name}-${var.environment}"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
 
-resource "azurerm_mssql_database" "example" {
+resource "azurerm_mssql_database" "db" {
   name         = "sqldb-${var.class_name}-${var.student_name}-${var.environment}"
-  server_id    = azurerm_mssql_server.example.id
+  server_id    = azurerm_mssql_server.server.id
   collation    = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
   max_size_gb  = 2
